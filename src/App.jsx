190e860283b26896bbdde78caffd85a146cf1a96
Cazares <chaotears@gmail.com>
2021-09-16 23:29:37 -0600

    Final product

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,19 @@ import Form from './components/Form'
 const App = () => {
 
   const [search, setSearch] = useState({ 
-    city: '',
-    country: ''
+    city: 'Juarez',
+    country: 'MX'
   });
   const [consulting, setConsulting] = useState(false);
   const [result, setResult] = useState({});
+  const [bg, setBg] = useState("");
   const [error, setError] = useState(false);
 
   const { city, country } = search;
   const consultAPI = async () => {
     if(consulting) {
       const appId = '31b33df22fe2b492d9b74843003438fe';
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${appId}&lang=sp`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${appId}&lang=sp`;
 
       const response = await fetch(url);
       const data = await response.json();
@@ -37,32 +38,34 @@ const App = () => {
   if(error){
     component = <Error result={result}/>
   }else{
-    component = <Weather result={result}/>
+    component = <Weather result={result} setBg={setBg}/>
   }
   
   useEffect(() => {
     consultAPI();
+    console.log(result);
     // eslint-disable-next-line
   },[consulting]);
 
+
   return ( 
-    <div className="bg-snowy bg-cover bg-bottom">
-      <div className="text-center flex flex-col h-screen content-between justify-between items-center bg-white/20 backdrop-blur-[2px]">
-
-        <div className="mt-10 text-lg md:text-4xl rounded-3xl">
-          {component}
+    <>
+      <div className={bg}>
+        <div className="text-center flex flex-col content-between justify-between items-center h-full bg-white/20 backdrop-blur-[2px] z-50">
+          <div className=" text-lg md:text-4xl rounded-xl p-10">
+            {component}
+          </div>
+          <div className="pb-10 font-semibold ">
+            <Form 
+              search={search}
+              setSearch={setSearch}
+              setConsulting={setConsulting}
+              className=""
+            />
+          </div>
         </div>
-        <div className="mb-10 font-semibold ">
-          <Form 
-            search={search}
-            setSearch={setSearch}
-            setConsulting={setConsulting}
-            className=""
-          />
-        </div>
-
       </div>
-    </div>
+    </>
   );
 }
  
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,35 +1,64 @@
 import React from 'react'
-const Weather = ({result}) => {
+const Weather = ({result, setBg}) => {
     if(!result.name) return null;
+    let temperature = parseFloat( result.main.temp - 273.15 ).toFixed(0);
+    let icon = result.weather[0].icon;
+    let direction;
+    let directionG=result.wind.deg;
+    if(directionG>=345 && directionG<=360) direction="Norte"
+    if(directionG>=0 && directionG<=22) direction="Norte"
+    if(directionG>=23 && directionG<=68) direction="Noreste"
+    if(directionG>=69 && directionG<=114) direction="Este"
+    if(directionG>=115 && directionG<=160) direction="Sureste"
+    if(directionG>=161 && directionG<=206) direction="Sur"
+    if(directionG>=207 && directionG<=252) direction="Suroeste"
+    if(directionG>=253 && directionG<=298) direction="Oeste"
+    if(directionG>=299 && directionG<=344) direction="Noroeste"
+    if(icon.includes("n")){
+        icon = icon.replace("n", "d")
+    }
+    switch(icon){
+        case '01d': setBg("bg-sunny          bg-cover bg-bottom absolute h-full w-screen");break;
+        case '02d': setBg("bg-sunny          bg-cover bg-bottom absolute h-full w-screen");break;
+        case '03d': setBg("bg-cloudy         bg-cover bg-bottom absolute h-full w-screen");break;
+        case '04d': setBg("bg-cloudy         bg-cover bg-bottom absolute h-full w-screen");break;
+        case '09d': setBg("bg-rainy          bg-cover bg-bottom absolute h-full w-screen");break;
+        case '10d': setBg("bg-rainy          bg-cover bg-bottom absolute h-full w-screen");break;
+        case '11d': setBg("bg-thunderstormy  bg-cover bg-bottom absolute h-full w-screen");break;
+        case '13d': setBg("bg-snowy          bg-cover bg-bottom absolute h-full w-screen");break;
+        case '50d': setBg("bg-foggy          bg-cover bg-bottom absolute h-full w-screen");break;
+        default:    setBg("bg-gradient-to-br from-gray-300 via-blue-200 to-blue-500 h-full w-screen");break;
+      }
+    let color;
+    if (temperature<=18){
+        color="text-blue-600"
+    }else{
+        if (temperature>=28){
+            color="text-red-600"
+        }else{
+            color="text-gray-700"
+        }
+    }
     return ( 
         <>
-            <h1 className="">{result.name}</h1>
+            <div className="flex mx-auto justify-center">
+                <h1 className="font-bold">{result.name},</h1> 
+                <p className="ml-3">{result.sys.country}</p>
+            </div>
+
+            <h2 className="mt-5"> Temperatura: <span className={`${color} font-bold`}> { temperature } ºC</span></h2>
+            <h2 className="capitalize underline mb-5 font-bold flex justify-center">{result.weather[0].description} <img src={`/${icon}.png`} alt="Weather icon" className="bg-gray-300 rounded-full ml-3" /></h2>
+
+            <h2 className="">Sensacion termica de: <span className="underline text-black font-bold">{ parseFloat( result.main.feels_like - 273.15 ).toFixed(0) }ºC</span></h2>
+            <h3 className="">Temperatura maxima: <span className="underline text-black font-bold">{ parseFloat( result.main.temp_max - 273.15 ).toFixed(0) }ºC</span></h3>
+            <h3 className="">Temperatura minima: <span className="underline text-black font-bold">{ parseFloat( result.main.temp_min - 273.15 ).toFixed(0) }ºC</span></h3>
             
-            <p className="">{result.sys.country}</p>
-
-            <h2 className="">
-            Temperatura: 
-            <span className=""> { parseFloat( result.main.temp - 273.15 ).toFixed(0) }</span>
-            ºC
-            </h2>
-
-            <h2 className="">{result.weather[0].description}</h2>
-
-            <h2 className="">Sensacion termica de { parseFloat( result.main.feels_like - 273.15 ).toFixed(0) }ºC</h2>
-
-            <div className="flex flex-col md:flex-row justify-center">
-            <h3 className="mx-2">Temperatura maxima: { parseFloat( result.main.temp_max - 273.15 ).toFixed(0) }ºC</h3>
-            <h3 className="mx-2">Temperatura minima: { parseFloat( result.main.temp_min - 273.15 ).toFixed(0) }ºC</h3>
-            </div>
-
-            <div className="flex flex-col md:flex-row justify-center">
-            <h3 className="mx-2">Humedad: { result.main.humidity }%</h3>
-            <h3 className="mx-2">Presion: { result.main.pressure }Mbar</h3>
-            </div>
-
-            <h2 className="">Velocidad del viento: {parseFloat( result.wind.speed * 3.6 ).toFixed(0)} Km/h</h2>
-
-            <h2 className="">Visibilidad: {parseFloat( result.visibility / 1000 ).toFixed(0)} Km</h2>
+            <h3 className="">Humedad: <span className="underline text-black font-bold">{ result.main.humidity }%</span></h3>
+            <h3 className="">Presion: <span className="underline text-black font-bold">{ result.main.pressure }Mbar</span></h3>
+            
+            <h2 className="">Velocidad del viento: <span className="underline text-black font-bold">{parseFloat( result.wind.speed * 3.6 ).toFixed(0)} Km/h </span></h2>
+            <h2 className="">Direccion del viento: <span className="text-red-800 font-bold">{direction}</span></h2>
+            <h2 className="">Visibilidad: <span className="underline text-black font-bold">{parseFloat( result.visibility / 1000 ).toFixed(0)} Km</span></h2>
         </>
      );
 }
